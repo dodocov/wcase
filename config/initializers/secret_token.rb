7f4932eaeedeba63892624559437f1c2ade413c1
fabiodoctorsys <fabiocariati@gmail.com>
2013-04-21 14:46:22 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wcase::Application.config.secret_token = 'ecf4ec614baca3ae5f3ad06b5b7644f67f28f71dac44e2a035e1db1e38c58cdd078d9a2b8be7f1ae2317438a7fae22a7dc0c4de2e893f683d7e2ead400cc6115'
